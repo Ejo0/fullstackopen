106b45e1e06bfdc7580d6dc29345ed174657a801
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-04-21 10:36:52 +0300

    5.2 add logout, login using local storage

--- osa5/bloglist-front/src/App.js
+++ osa5/bloglist-front/src/App.js
@@ -16,11 +16,20 @@ const App = () => {
     })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('blogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logginf in with', username, password)
     try {
       const user = await loginService.login({username, password})
+
+      window.localStorage.setItem('blogAppUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -29,11 +38,17 @@ const App = () => {
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('blogAppUser')
+    setUser(null)
+  }
+
   return (user)
     ? (
     <div>
       <h2>Blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout} >Logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
