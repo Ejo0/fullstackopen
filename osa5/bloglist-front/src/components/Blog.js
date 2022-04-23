2be00d2cf528e4aeab02d8064851f5c818016edc
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-04-23 10:51:38 +0300

    5.7 add full view option for blogs, simple styling

--- osa5/bloglist-front/src/components/Blog.js
+++ osa5/bloglist-front/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+    const [fullView, setFullView] = useState(false)
+
+    const toggleFullView = () => setFullView(!fullView)
+
+    return (
+        <div className="blog">
+            {blog.title}; {blog.author} 
+            <button onClick={toggleFullView}>{fullView ? 'Hide' : 'View'}</button><br/>
+            {fullView
+            ? (
+                <div>
+                    {blog.url}<br/>
+                    Likes: {blog.likes} 
+                    <button>Like</button><br/>
+                    {blog.user.name}      
+                </div>
+            )
+            : null}
+        </div>
+    )
+}
 
 export default Blog
